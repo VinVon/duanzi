529501d3f40825730f83dcd3abdb63837cac5d77
VinVon <642751580@qq.com>
2017-08-13 17:57:46 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/duanzi/MainActivity.java
@@ -0,0 +1,21 @@
+package com.demo.duanzi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
